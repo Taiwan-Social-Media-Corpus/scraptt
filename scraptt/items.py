d2a0f61a65987d9571a8e54dcf45a03ead9bad0c
aji <amigcamel@gmail.com>
2017-09-18 14:32:18 +0800

    define item to be scraped

--- scraptt/items.py
+++ scraptt/items.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
+"""Scrapy scraped items."""
 import scrapy
 
 
-class ScrapttItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PostItem(scrapy.Item):
+    '''Item for "POST".'''
+    author = scrapy.Field()
+    time = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    content = scrapy.Field()
