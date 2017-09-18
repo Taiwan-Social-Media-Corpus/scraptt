71c3903b4c7012cb93752b810226ef3dda37526d
aji <amigcamel@gmail.com>
2017-09-18 23:05:12 +0800

    1. rename crawler `index` to `ptt` 2. add `parse_post`

--- scraptt/spiders/ptt.py
+++ scraptt/spiders/ptt.py
@@ -2,14 +2,19 @@
 """Main crawler."""
 import re
 from datetime import datetime
+from itertools import groupby
 
 import scrapy
+import dateutil.parser as dp
+
+from .parsers.post import mod_content, extract_author
+from .parsers.comment import comment_counter, remove_ip
 
 
 class PttSpider(scrapy.Spider):
     """Crawler for PTT."""
 
-    name = 'index'
+    name = 'ptt'
     allowed_domains = ['ptt.cc']
 
     def __init__(self, *args, **kwargs):
@@ -49,5 +54,103 @@ class PttSpider(scrapy.Spider):
             if time.date() < self.since:
                 return
             print('+ ', title, href, time)
+            yield scrapy.Request(href, self.parse_post)
         prev_url = response.dom('.btn.wide:contains("上頁")').attr('href')
         yield scrapy.Request(prev_url, self.parse_index)
+
+    def parse_post(self, response):
+        """Parse PTT post (PO文)."""
+        content = (
+            response.dom('#main-content')
+            .clone()
+            .children()
+            .remove('span[class^="article-meta-"]')
+            .remove('div.push')
+            .end()
+            .html()
+        )
+        meta = dict(
+            (_.text(), _.next().text())
+            for _
+            in response.dom('.article-meta-tag').items()
+        )
+        ref = {
+            '作者': 'author',
+            '時間': 'published',
+            '標題': 'title',
+            '看板': 'board',
+            '站內': 'board'
+        }
+        post = dict()
+        post['content'] = mod_content(content)
+        post['url'] = response.url
+        meta_mod = dict()
+        for k in meta.keys():
+            if k in ref:
+                meta_mod[ref[k]] = meta[k]
+            else:
+                raise Exception(f'Unknown key: {k}')
+        comments = []
+        for _ in response.dom('.push').items():
+            comment = {
+                'type': _('.push-tag').text(),
+                'author': extract_author(_('.push-userid').text()),
+                'content': _('.push-content').text().lstrip(' :'),
+                'time': {
+                    'published': _('.push-ipdatetime').text(),
+                    'crawled': datetime.now().replace(microsecond=0),
+                }
+            }
+            comments.append(comment)
+
+        post.update(meta_mod)
+        post['author'] = extract_author(post['author'])
+        post['time'] = {
+            'published': dp.parse(post.pop('published'))
+        }
+        post['comments'] = comments
+
+        # Merge comments with consecutive comments with the same author.
+        con = []
+        for author, group in groupby(comments, key=lambda x: x['author']):
+            d = {}
+            for comment in group:
+                if d:
+                    d['content'] += comment['content']
+                else:
+                    d = comment
+            con.append(d)
+
+        # add YEAR to comments
+        year = post['time']['published'].year
+        latest_month = post['time']['published'].month
+        for comment in comments:
+            published = dp.parse(remove_ip(comment['time']['published']))
+            if published.month < latest_month:
+                year += 1
+            comment['time']['published'] = published.replace(year=year)
+            latest_month = published.month
+
+        # quotes
+        msg = post['content']
+        qs = re.findall('※ 引述.*|\n: .*', msg)
+        for q in qs:
+            msg = msg.replace(q, '')
+        qs = '\n'.join([i.strip('\n') for i in qs])
+        post['content'] = msg.strip('\n ')
+        if qs:
+            post['quote'] = qs
+        post['time']['crawled'] = datetime.now().replace(microsecond=0)
+
+        # 推噓文數量
+        post.update(
+            {'count': comment_counter(post['comments'])}
+        )
+        print('-------------')
+        import json
+        print(
+            json.dumps(
+                post, indent=4, ensure_ascii=False, default=lambda x: str(x)
+            )
+        )
+        print('-------------')
