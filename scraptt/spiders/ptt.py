fcad570c994361974fd104d2384862fd118d8adc
aji <amigcamel@gmail.com>
2017-09-18 18:21:28 +0800

    1. add parameter `since` 2. loop for index pages

--- scraptt/spiders/ptt.py
+++ scraptt/spiders/ptt.py
@@ -15,9 +15,16 @@ class PttSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         """__init__ method.
 
-        :params: boards: comma-separated board list
+        :param: boards: comma-separated board list
+        :param: since: start crawling from this date (format: YYYYMMDD)
         """
         self.boards = kwargs.pop('boards').strip().split(',')
+        since = kwargs.pop('since', None)
+        self.since = (
+            datetime.strptime(since, '%Y%m%d').date()
+            if since
+            else datetime.now().date()
+        )
 
     def start_requests(self):
         """Request handler."""
@@ -28,10 +35,19 @@ class PttSpider(scrapy.Spider):
     def parse(self, response):
         """Parse DOM."""
         # exclude "置底文"
-        topics = response.dom('.r-list-sep').prev_all('.r-ent .title a')
-        for topic in topics.items():
+        item_css = '.r-ent .title a'
+        if response.url.endswith('index.html'):
+            topics = response.dom('.r-list-sep').prev_all(item_css)
+        else:
+            topics = response.dom(item_css)
+        # reverse order to conform to timeline
+        for topic in reversed(list(topics.items())):
             title = topic.text()
             href = topic.attr('href')
             timestamp = re.search(r'(\d{10})', href).group(1)
             time = datetime.fromtimestamp(int(timestamp))
-            print(title, href, time)
+            if time.date() < self.since:
+                return
+            print('+ ', title, href, time)
+        prev_url = response.dom('.btn.wide:contains("上頁")').attr('href')
+        yield scrapy.Request(prev_url, self.parse)
